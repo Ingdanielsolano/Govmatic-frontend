d9518356a26df6e76cea15343fd49a02202a9325
ingdanielsolano <ds.development.app@gmail.com>
2020-06-17 13:27:00 -0500

    blank

--- src/common/config.js
+++
@@ -1,3 +0,0 @@
-export const ENV = "local";
-export const API_URL = "http://govmatic-nest.herokuapp.com/v1/";
-// export const API_URL = "http://localhost:3000/v1/";
--- src/store/sagas.js
+++
@@ -1,9 +0,0 @@
-import { fork, all } from 'redux-saga/effects';
-
-import grantListSaga from '../services/grantList/sagas';
-
-export default function* rootSaga() {
-    yield all([
-        fork(grantListSaga)
-    ]);
-}
